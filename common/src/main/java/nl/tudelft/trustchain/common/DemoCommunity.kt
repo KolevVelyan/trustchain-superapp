486f546d35a0f6a53c90283dcaf0b0b584e357f4
Tsvetomir Hristov <t.hristov@student.tudelft.nl>
2024-03-03 17:21:04 +0100

    adding file endpoint

--- common/src/main/java/nl/tudelft/trustchain/common/DemoCommunity.kt
+++ common/src/main/java/nl/tudelft/trustchain/common/DemoCommunity.kt
@@ -8,8 +8,10 @@ import nl.tudelft.ipv8.android.IPv8Android
 import nl.tudelft.ipv8.attestation.trustchain.TrustChainCommunity
 import nl.tudelft.ipv8.messaging.Address
 import nl.tudelft.ipv8.messaging.Packet
+import nl.tudelft.ipv8.messaging.payload.IntroductionRequestPayload
 import nl.tudelft.ipv8.messaging.payload.IntroductionResponsePayload
 import nl.tudelft.ipv8.messaging.payload.PuncturePayload
+import nl.tudelft.ipv8.messaging.payload.PunctureRequestPayload
 import java.util.Date
 
 class DemoCommunity : Community() {
@@ -47,6 +49,7 @@ class DemoCommunity : Community() {
     object MessageId {
         const val PUNCTURE_TEST = 251
         const val OPEN_PORT = 252
+        const val OPEN_PORT_RESPONSE = 253
     }
 
     fun sendPuncture(
@@ -64,13 +67,14 @@ class DemoCommunity : Community() {
     ) {
         val payload = PuncturePayload(myEstimatedLan, myEstimatedWan, id)
         val packet = serializePacket(MessageId.OPEN_PORT, payload, sign = false)
-        endpoint.send(address, packet)
+        endpoint.fileEndpoint?.send(address, packet)
     }
 
     // RECEIVE MESSAGE
     init {
         messageHandlers[MessageId.PUNCTURE_TEST] = ::onPunctureTest
         messageHandlers[MessageId.OPEN_PORT] = ::onOpenPort
+        messageHandlers[MessageId.OPEN_PORT_RESPONSE] = ::onOpenPortResponse
     }
 
     private fun onPunctureTest(packet: Packet) {
@@ -79,7 +83,21 @@ class DemoCommunity : Community() {
     }
 
     private fun onOpenPort(packet: Packet) {
-        val payload = packet.getPayload(PuncturePayload.Deserializer)
-    System.out.print(payload.toString())
+        val payload = packet.getPayload(PunctureRequestPayload.Deserializer)
+        if (packet.source is IPv4Address) {
+            val target = if (payload.wanWalkerAddress.ip == myEstimatedWan.ip) {
+                // They are on the same LAN, puncture should not be needed, but send it just in case
+                payload.lanWalkerAddress
+            } else {
+                payload.wanWalkerAddress
+            }
+
+            val packet_ = createPuncture(myEstimatedLan, myEstimatedWan, payload.identifier)
+            endpoint.fileEndpoint?.send(target, packet_)
+        }
+    }
+
+    private fun onOpenPortResponse(packet: Packet) {
+        System.out.print("Helloooo" + packet.toString());
     }
 }
